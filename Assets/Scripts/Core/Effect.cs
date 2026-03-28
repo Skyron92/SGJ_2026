825efe444c6e1bb6057450fbc639e4e2654b56d8
Skyron <courtier.leo@gmail.com>
2026-03-28 17:39:03 +0100

    Improve effects Ph

--- Assets/Scripts/Core/Effect.cs
+++ Assets/Scripts/Core/Effect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Gameplay/Effect", fileName = "Effect", order = 0)]
 public class Effect : ScriptableObject
 {
-    [SerializeField, Range(0,1000)] public int MaxHeat;
-    public bool SupportUv,SupportPulsed;
-    [SerializeField, Range(0,14)] public int MaxPh, MinPh;
+    [Range(0,1000)] public int MaxHeat;
+    [Range(-1,1)] public int SupportUv,SupportPulsed;
+    [Range(0,14)] public int MaxPh, MinPh;
 }
