24d48a0a3c9c8a29bca8294fc20c3baf106e6e94
Skyron9 <courtier.leo@gmail.com>
2026-03-31 18:48:10 +0200

    Animated text

--- Assets/Scripts/UI/BoxHelper.cs
+++ Assets/Scripts/UI/BoxHelper.cs
@@ -1,13 +1,21 @@
-﻿using TMPro;
+﻿using System.Collections;
+using System.Text;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace UI
 {
-    public class BoxHelper : MonoBehaviour
+    public class BoxHelper : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] UnityEvent onOpen, onClose;
         [SerializeField] TextMeshProUGUI textMeshProUGUI;
+        [SerializeField, Range(0,0.1f)] private float offset = 0.05f;
+        private bool hasClicked;
+        private Coroutine current;
+        private StringBuilder stringBuilder = new();
+        
 
         public void Open()
         {
@@ -19,6 +27,38 @@ namespace UI
             onClose?.Invoke();
         }
 
-        public void SetText(string text) => textMeshProUGUI.text = text;
+        public void SetText(string text)
+        {
+            if (current != null) StopCoroutine(current);
+            current = StartCoroutine(AnimateText(text));
+        }
+
+        private IEnumerator AnimateText(string text)
+        {
+            yield return new WaitForEndOfFrame();
+            textMeshProUGUI.text = "";
+            stringBuilder.Clear();
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                var t = text[i];
+                if (hasClicked) {
+                    if (i > 0) textMeshProUGUI.text = text;
+                    hasClicked = false;
+                    stringBuilder.Clear();
+                    yield break;
+                }
+
+                stringBuilder.Append(t);
+                textMeshProUGUI.text = stringBuilder.ToString();
+                yield return new WaitForSeconds(offset);
+            }
+            stringBuilder.Clear();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            hasClicked = true;
+        }
     }
 }
